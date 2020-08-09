1d07c12a1b6cc74fa7a67c7919ff2cb884842d02
mapdemployee <assel.sapasheva@omnisci.com>
2020-08-08 20:37:24 -0700

    added second circle

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import './App.css';
 import { Circle } from 'react-shapes';
 import { Ellipse } from 'react-shapes';
+import Circle2 from './components/Circle2'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Circle r={200} fill={{color: '#19F1EB'}} stroke={{color:'#22D8D3'}} strokeWidth={35} />
+        <div
+           className = "App-Circle2"  
+        > 
+          <Circle2 />
+        </div>
+        
        
        <h1>React-Shape!</h1>
       </header>
---
+++ src/components/Circle2.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Circle } from 'react-shapes';
+import { isAbsolute } from 'path';
+
+function Circle2(){
+    
+    return <Circle r={100} fill={{color: '#FFFFFF'}} stroke={{color:'#3DE1DC '}} strokeWidth={25} />
+}
+
+export default Circle2

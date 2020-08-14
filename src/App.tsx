02ba66ebb8a0ad4860d413553579734e6015f5d7
mapdemployee <assel.sapasheva@omnisci.com>
2020-08-14 12:08:46 -0700

    added slider with circle

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Circle } from 'react-shapes';
 import { Ellipse } from 'react-shapes';
-import Circle2 from './components/Circle2'
+import Circle2 from './components/Circle2';
+import { Slider } from '@rmwc/slider';
+import '@material/slider/dist/mdc.slider.css';
 
 function App() {
+  const [radius, setRadius] =useState(150)
   return (
     <div className="App">
       <header className="App-header">
-        <Circle r={200} fill={{color: '#19F1EB'}} stroke={{color:'#22D8D3'}} strokeWidth={35} />
-        <div
+        <div className="App-controls">
+        {radius}
+          {/* <className = "App-Circle1"> */}
+          
+        <Slider className= "circle-slider" min={20} max={200} value={radius} discrete step={1} 
+         onInput={(event) => {
+          const newRadius = event.detail.value
+
+          setRadius(newRadius)
+        }}
+        />
+        </div>
+        <div>
+  
+        <Circle r={radius} fill={{color: '#19F1EB'}} stroke={{color:'#22D8D3'}} strokeWidth={35} />
+        </div>
+        </header>
+        {/* <div
            className = "App-Circle2"  
         > 
           <Circle2 />
         </div>
+       */}
         
        
        <h1>React-Shape!</h1>
-      </header>
+    
     </div>
   );
 }
